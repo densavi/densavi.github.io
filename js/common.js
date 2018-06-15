d25ac11870f80a99cf6218250f68f7db3a5520c7
Denis <denis.savitsky@wdg.com.ua>
2018-06-15 11:15:49 +0300

    main page

---
+++ js/common.js
@@ -0,0 +1,68 @@
+$(function() {
+
+	function menuHamburger() {
+		$('.header__menu').click(function(){
+			$(this).toggleClass('open');
+		});
+	} menuHamburger();
+
+	function tabs() {
+		$('.provide-serc__caption').on('click', 'li:not(.active)', function() {
+		    $(this)
+		      .addClass('active').siblings().removeClass('active')
+		      .closest('.provide-serc__tabs').find('.provide-serc__content').removeClass('active').eq($(this).index()).addClass('active');
+		});
+	} tabs();
+
+	function slider() {
+		$('.slider').slick({
+			slidesToShow: 2,
+			dots: true,
+			arrows: false,
+			adaptiveHeight: true,
+			responsive: [
+		    {
+		      breakpoint: 767,
+		      settings: {
+		        slidesToShow: 1
+		      }
+		    }
+		    ]
+		});
+		$('.slider__arrow--next').on('click', function(){
+		  $('.slider').slick('slickNext');
+		});
+		$('.slider__arrow--prev').on('click', function(){
+		  $('.slider').slick('slickPrev');
+		});
+	} slider();
+
+	function mobileSlider() {
+
+
+		$('.partners__list').slick({
+			slidesToShow: 6,
+			dots: false,
+			responsive: [
+			{
+			  breakpoint: 1000,
+			  settings: {
+			    slidesToShow: 3,
+			    dots: true
+			  }
+			},
+			{
+			  breakpoint: 500,
+			  settings: {
+			    slidesToShow: 2,
+			    dots: true
+			  }
+			}
+			]
+		});
+		
+
+	} mobileSlider();
+
+
+});
